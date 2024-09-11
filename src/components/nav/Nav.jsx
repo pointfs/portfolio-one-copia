ae02654d4567932543301a67a0e53ae1eaf48c3b
fatima <puntod81@gmail.com>
2024-09-11 19:58:11 +0200

    small-cube-inspiration

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ButtonTech from '../../components/buttoms/buttomtech/ButtomTech';
 import ButtonProjects from '../../components/buttoms/buttomprojects/ButtomProjects';
 import ButtonHome from '../../components/buttoms/buttomhome/ButtomHome';
@@ -6,23 +6,27 @@ import ButtonInspiration from '../../components/buttoms/buttominspiration/Buttom
 
 import hacker3 from '../../assets/icons/hacker3.webp';
 
-
 const Nav = () => {
-  return (
-    
-      <div className="navbar">
-        <img className='logo-nav' src={hacker3} alt="logo" />
+  const [menuOpen, setMenuOpen] = useState(false);
 
-        <ul className="menu">
-          <li><ButtonHome /></li>
-          <li><ButtonProjects /></li>
-          <li><ButtonTech /></li>
-          <li><ButtonInspiration /></li>
-          <button className="button-mail-nav" a href="mailto:fatimacosme81@gmail.com">Contacto</button>
-        </ul>
-        
-      </div>
-    
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <div className="navbar">
+      <img className='logo-nav' src={hacker3} alt="logo" />
+      <button className="hamburger" onClick={toggleMenu}>
+        &#9776;
+      </button>
+      <ul className={`menu ${menuOpen ? 'open' : ''}`}>
+        <li><ButtonHome /></li>
+        <li><ButtonProjects /></li>
+        <li><ButtonTech /></li>
+        <li><ButtonInspiration /></li>
+        <button className="button-mail-nav" a href="mailto:fatimacosme81@gmail.com">Contacto</button>
+      </ul>
+    </div>
   );
 }
 
